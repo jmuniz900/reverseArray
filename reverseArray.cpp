b1413514b8214bea5ed41270132a1c1f4fdafc8e
jmuniz900 <jmuniz900@cs3004b.acad.fullerton.edu>
2019-02-05 17:17:14 -0800

    Created code to reverse arrays

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -2,5 +2,23 @@
 
 using namespace std;
 
-int main{
+int SIZE = 4; //global size
+
+int reverseArray(int &array){
+  int count = SIZE;
+  int arrayR[SIZE];
+
+  while(count > 0){
+    for(int i = 0; i < SIZE; i++){
+      arrayR[i] = array[count];
+    }
+    count--;
+  }
+  return arrayR;
+}
+
+int main(){
+  int array[SIZE] = {1, 2, 3, 4};
+
+  reverseArray(array);
 }
