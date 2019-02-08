f0faec39ced591c9b7354104b2bb0ff3c7c158b2
jsmith74 <jsmith74@cs3004b.acad.fullerton.edu>
2019-02-07 17:16:01 -0800

    minor changes like spacing

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 void reverseArray(int (&thearray)[4], int &thesize){
   int count = thesize - 1;
+
   cout << "After: ";
+
 for(int i = 0; i < thesize; i++){
   cout << thearray[count] << " ";
   count--;
@@ -17,6 +19,7 @@ int main(){
   int myarray[4] = {1, 2, 3, 4};
 
   cout << "Before: ";
+  
   for(int i = 0; i < sizeOfArray; i++){
     cout << myarray[i] << " ";
   }
